ebc782808e1a821cbe362f2dd8ec8c499f6cbae1
sadegh <akbarisadegh@gmail.com>
2023-02-25 14:34:56 +0330

    create app

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <a href="/" >AIEX</a>
+      </header>
+      <main>products</main>
+    </div>
+  );
+}
+
+export default App;
